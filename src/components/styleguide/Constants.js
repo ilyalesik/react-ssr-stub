81584d9180788e4732585aeed65400cdcbfaa4fe
Ilia Lesik <ilialesik@gmail.com>
2017-12-26 12:18:03 +0300

    Constants

---
+++ src/components/styleguide/Constants.js
@@ -0,0 +1,17 @@
+export const colors = {
+    white: "#ffffff",
+
+    errorColor: "#d0021b"
+};
+
+export const media = {
+    tabletLandscape: "1339px",
+    tabletPortrait: "1023px",
+    phone: "767px"
+};
+
+export const zIndexes = {
+    background: -1,
+    header: 50,
+    headerMenu: 60
+};
