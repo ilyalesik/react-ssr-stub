cdd2e3116514b1c08b4cfecb6b1cad37be8726f9
Ilia Lesik <ilialesik@gmail.com>
2017-12-26 14:55:26 +0300

    HomePage style

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -5,9 +5,13 @@ const HomePageContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    height: 100vh;
 `;
 
-const HomePageTitle = styled.h1``;
+const HomePageTitle = styled.h1`
+    font-family: Helvetica;
+    font-size: 30px;
+`;
 
 export default class HomePage extends React.PureComponent {
     render() {
